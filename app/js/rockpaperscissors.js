9185236cc651a40d23a5dc4d1075e5c2c581f47e
Kory Campbell <KoryJCampbell@new-host-3.home>
2015-10-07 02:43:33 -0400

    Unit 5 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -38,11 +38,31 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    var winner;
+
+    if (playerMove === "rock" && computerMove === "scissors") {
+        winner = "player";
+    } else if (playerMove === "scissors" && computerMove === "rock") {
+        winner = "computer";
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        winner = "player";
+    } else if (playerMove === "paper" && computerMove === "scissors") {
+        winner = "computer";
+    } else if (playerMove === "paper" && computerMove === "rock") {
+        winner = "player";
+    } else if (playerMove === "rock" & computerMove === "paper") {
+        winner = "computer";
+    }
+
+    else if (playerMove === computerMove) {
+        winner = "tie"
+    } else {
+        winner = "error";
+    }
+
     return winner;
 }
 
@@ -50,8 +70,26 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+
+    while(playerWins < 5 && computerWins < 5) {
+            var playerMove = getPlayerMove();
+            var computerMove = getComputerMove();
+            var winner = getWinner(playerMove,computerMove);
+        if (winner === "player") {
+        playerWins  += 1;
+        console.log("Player wins");
+        } else if (winner === "computer") {
+            computerWins += 1;
+            console.log("Computer wins");
+        } else {
+            
+        }
+
+    console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+    console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    }
+    
+
     return [playerWins, computerWins];
 }
 
